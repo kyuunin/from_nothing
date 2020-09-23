8e93efb9a3b1128a727749b13b789971a8b40ac8
David Hedtke <david.hedtke@vescon.com>
2020-09-23 17:09:21 +0200

    Implemented first enemy

---
+++ Assets/Enemies/Scripts/DamageEnemy.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class DamageEnemy: MonoBehaviour
+{
+    [SerializeField]
+    private int life;
+
+    public void TakeDamage(int damage)
+    {
+        life -= damage;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (life <= 0)
+            Destroy(gameObject);
+    }
+}
---
+++ Assets/Enemies/Scripts/DamagePlayer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UnityEngine;
+
+public class DamagePlayer : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject Player;
+
+    private bool _isInPlayerCollision = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!_isInPlayerCollision)
+            return;
+
+        if (DamageValues.PlayerIsSafeStartTime.HasValue
+            && (DateTime.Now - DamageValues.PlayerIsSafeStartTime.Value) < DamageValues.PlayerIsSafeTimeSpan)
+            return;
+
+        DamageValues.PlayerIsSafeStartTime = DateTime.Now;
+
+        DamageValues.LifeOfPlayer--;
+
+        if (DamageValues.LifeOfPlayer == 0)
+            Destroy(Player);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        _isInPlayerCollision = collision.gameObject == Player;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject == Player)
+            _isInPlayerCollision = false;
+    }
+}
