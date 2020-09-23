63316a789b0164ff80f17bcb5ccf6534200ceeb7
Your Name <nholigha@students.uni-mainz.de>
2020-09-23 22:18:52 +0200

    sudoku

---
+++ Assets/Ability/LightningAttack.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+
+public class LightningAttack : MonoBehaviour
+{
+    public int cooldown;
+    private DateTime? timer = null;
+
+    [SerializeField]
+    private GameObject projectile;
+    private int _dannyDirection;
+
+    void Start()
+    {
+        timer = DateTime.Now;
+    }
+
+    void Update()
+    {
+        var isInputAggressive = Input.GetButtonDown("FlameAtak");
+        if ((DateTime.Now - timer).Value.TotalMilliseconds < cooldown)
+            return;
+        //Jot Dora
+        if (isInputAggressive)
+        {
+            if (!Utilities.IsGrounded())
+                return;
+            CommonValuesStore.CommonValues.RigidBodyOfPlayer.velocity = new Vector2(0, 0);
+            _dannyDirection = CommonValuesStore.CommonValues.DannyDirection ? 1 : -1;
+            if (_dannyDirection == -1)
+                Instantiate(projectile, CommonValuesStore.CommonValues.RigidBodyOfPlayer.transform.position + new Vector3(_dannyDirection * 0.7f, 0, 0), new Quaternion(0, 180, 0, 0));
+
+            else
+                Instantiate(projectile, CommonValuesStore.CommonValues.RigidBodyOfPlayer.transform.position + new Vector3(_dannyDirection * 0.7f, 0, 0), Quaternion.identity);
+            timer = DateTime.Now;
+        }
+    }
+}
