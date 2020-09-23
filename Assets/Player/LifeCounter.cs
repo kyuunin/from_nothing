876a8c7d319b0579f21a78086bf84840e05a6123
David Hedtke <david.hedtke@vescon.com>
2020-09-23 18:59:01 +0200

    Added live counter for player

---
+++ Assets/Player/LifeCounter.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using UnityEngine;
+
+public class LifeCounter : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] Hearts;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        var activeHearts = Hearts
+            .Select((x, i) => new { Heart = x, Index = i })
+            .Where(x => x.Index < DamageValues.LivesOfPlayer)
+            .ToList();
+
+        activeHearts
+            .ForEach(x => x.Heart.SetActive(true));
+
+        var inactiveHearts = Hearts
+            .Select((x, i) => new { Heart = x, Index = i })
+            .Where(x => x.Index >= DamageValues.LivesOfPlayer)
+            .ToList();
+
+        inactiveHearts
+            .ForEach(x => x.Heart.SetActive(false));
+    }
+}
