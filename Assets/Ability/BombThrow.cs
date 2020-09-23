8d6bc73297bb83022ca04d1c2129f91ad250c3ca
Your Name <nholigha@students.uni-mainz.de>
2020-09-23 23:58:43 +0200

    Added bomb throwing

---
+++ Assets/Ability/BombThrow.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using UnityEngine;
+
+public class BombThrow : MonoBehaviour
+{
+    private DateTime? timer = null;
+    public int cooldown;
+    public float throwStrength;
+
+    [SerializeField]
+    private GameObject projectile;
+    private int _dannyDirection;
+
+    void Start()
+    {
+        timer = DateTime.Now;
+    }
+
+    void Update()
+    {
+        var isInputAggressive = Input.GetButtonDown("BombThrow");
+        GameObject bomb;
+        if ((DateTime.Now - timer).Value.TotalMilliseconds < cooldown)
+            return;
+        //Jot Dora
+        if (isInputAggressive)
+        {
+            Vector2 velo;
+            //top right
+            if (Input.GetAxisRaw("Aim") > 0.1)
+                velo = new Vector2(1, 2);
+            else if (Input.GetAxisRaw("Aim") < -0.1)
+                velo = new Vector2(0, 0);
+            else
+                velo = new Vector2(1, 0);
+            velo *= throwStrength;
+            _dannyDirection = CommonValuesStore.CommonValues.DannyDirection ? 1 : -1;
+            if (_dannyDirection == -1) {
+                velo.x *= -1;
+                bomb = Instantiate(projectile, CommonValuesStore.CommonValues.RigidBodyOfPlayer.transform.position + new Vector3(_dannyDirection * 0.5f, 0, 0), new Quaternion(0, 180, 0, 0));
+            }
+            else {
+                bomb= Instantiate(projectile, CommonValuesStore.CommonValues.RigidBodyOfPlayer.transform.position + new Vector3(_dannyDirection * 0.5f, 0, 0), Quaternion.identity);
+            }
+            bomb.GetComponent<Rigidbody2D>().velocity = velo;
+            timer = DateTime.Now;
+        }
+    }
+}
---
+++ Assets/Stuff/BombProjectileScript.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using UnityEngine;
+
+public class BombProjectileScript : MonoBehaviour
+{
+    public int damage;
+    public int lifetime;
+
+    private int _dannyDirection;
+    private DateTime? timer = null;
+    private float radius;
+    private DateTime? explodedTimer = null;
+
+    private void Start()
+    {
+        timer = DateTime.Now;
+        _dannyDirection = CommonValuesStore.CommonValues.DannyDirection ? 1 : -1;
+        radius = GetComponent<CircleCollider2D>().radius*3;
+    }
+
+    //destroy projectile and calc damage
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log(collision.gameObject.layer);
+        if (collision.gameObject.layer == 8 || collision.gameObject.layer == 9)
+            return;
+        Debug.Log(collision);
+        //deal damage
+        //explosion
+        GetComponent<CircleCollider2D>().radius = radius;
+        var enemy = collision.gameObject.GetComponent<DamageEnemy>();
+        if (enemy != null)
+            enemy.TakeDamage(damage);
+        //selfdestruct
+        explodedTimer = DateTime.Now;
+    }
+
+    // shoot it, bop it, drop it
+    void Update()
+    {
+        if (explodedTimer != null && (DateTime.Now - explodedTimer).Value.TotalMilliseconds > 50)
+            Destroy(gameObject);
+        if ((DateTime.Now - timer).Value.TotalMilliseconds < lifetime)
+            return;
+        else
+        {
+            GetComponent<CircleCollider2D>().radius = radius;
+            Destroy(gameObject);
+        }
+    }
+}
