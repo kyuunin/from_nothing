74533721743bd1acf9017f20e19ed1060a7ffb39
Christian Neumann <cneumann@students.uni-mainz.de>
2020-09-23 16:41:50 +0200

    Basic Level

---
+++ Assets/Objects/DoorOpener.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpener : MonoBehaviour
+{
+    public Sprite ClosedSprite;
+    public Sprite OpenSprite;
+    public bool IsOpen = false;
+    private SpriteRenderer render = null;
+    private BoxCollider2D collider = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        collider = GetComponent<BoxCollider2D>();
+        render = GetComponent<SpriteRenderer>();
+        render.sprite = IsOpen ? OpenSprite : ClosedSprite;
+        collider.enabled = !IsOpen;
+    }
+    public void SetOpen(bool open) {
+        IsOpen = open;
+        render.sprite = IsOpen ? OpenSprite : ClosedSprite;
+        collider.enabled = !IsOpen;
+    }
+}
