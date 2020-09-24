ff9b909c3128acd71bb274b130fd499b9d0759f0
Your Name <nholigha@students.uni-mainz.de>
2020-09-24 14:58:49 +0200

    sudoku

---
+++ Assets/ENVIRONMENT & TRAPS/checkpointScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class checkpointScript : MonoBehaviour
+{
+    //press down to save
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (Input.GetAxisRaw("Aim")< -0.1)
+        {
+
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
