f2f510428c5b5389790ce1d8b67637ff73880655
Christian Neumann <cneumann@students.uni-mainz.de>
2020-09-25 12:04:08 +0200

    Made heart

---
+++ Assets/Enemies/Scripts/HealPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealPlayer : MonoBehaviour
+{
+    public int HealValue;
+    void Heal(int lives)
+    {
+        ValuesStore.CommonValues.LivesOfPlayer += lives;
+        if(ValuesStore.CommonValues.LivesOfPlayer > DamageValues.DefaultLivesOfPlayer)
+        {
+            ValuesStore.CommonValues.LivesOfPlayer = DamageValues.DefaultLivesOfPlayer;
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("hit");
+        Heal(HealValue);
+        Destroy(gameObject);
+    }
+}
