b3783ac3a004e532ffc8e050674057a7be574272
Your Name <nholigha@students.uni-mainz.de>
2020-09-25 13:41:30 +0200

    a

--- Assets/Objects/ButonController.cs
+++ Assets/Objects/ButonController.cs
@@ -5,13 +5,8 @@ using UnityEngine;
 public class ButonController : MonoBehaviour
 {
     public GameObject Door;
-    private IDoor _door;
-    private void Start()
-    {
-        _door = Door.GetComponent<IDoor>();
-    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        _door.SetOpen(true);
+        Door.GetComponent<IDoor>().SetOpen(true);
     }
 }
